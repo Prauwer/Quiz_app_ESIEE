fba4d7817c4c8946cf751c4d8d55a25084e2ed9f
SAADA Zackary <ZSA4@dsone.3ds.com>
2025-06-26 20:37:43 +0200

    added authentification

--- quiz_api/app.py
+++ quiz_api/app.py
@@ -1,8 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+import jwt
+from datetime import datetime, timedelta
+import hashlib
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a1b9f8c7e6d5a4b3c2d1f0e9d8c7b6a5a4b3c2d1f0e9'
 CORS(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    payload = request.get_json()
+    # Le hash MD5 du mot de passe 'iloveflask'
+    admin_password_hash = 'd278077bbfe7285a144d4b5b11adb9cf'
+
+    if payload and 'password' in payload:
+        # Convertir le mot de passe reçu en MD5
+        password_from_request = payload['password']
+        hashed_password = hashlib.md5(password_from_request.encode()).hexdigest()
+
+        # Comparer les hashs
+        if hashed_password == admin_password_hash:
+            token = jwt.encode({
+                'exp' : datetime.utcnow() + timedelta(minutes=30)
+            }, app.config['SECRET_KEY'], algorithm="HS256")
+            return {"token": token}, 200
+            
+    return 'Unauthorized', 401
 
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
