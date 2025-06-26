a8528e59322c20160d25404e7de3fd24ec9fec43
SAADA Zackary <ZSA4@dsone.3ds.com>
2025-06-26 20:09:04 +0200

    premier endpoint

--- quiz_api/app.py
+++ quiz_api/app.py
@@ -4,9 +4,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
+
 @app.route('/')
 def hello_world():
-	x = 'world!\n'
+	x = 'world'
 	return f"Hello, {x}"
 
 if __name__ == "__main__":
